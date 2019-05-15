9fc0f2ebdb3d2a62bc3b9c909fc4713cba8cbb94
Michael Williams <michaelwilliams6@hotmail.com>
2019-05-15 17:39:57 +0200

    Merge branch 'develop' of https://github.com/fask0/Project-Exposure into develop
    # Conflicts:
    #	.gitattributes
    #	Project Exposure/Assets/FMODStudioCache.asset
    #	Project Exposure/Assets/Scenes/Mike Scenes/Mike Test Scene.unity

---
+++ Project Exposure/Assets/Scripts/Fish/SchoolFish/SwarmActivator.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwarmActivator : MonoBehaviour
+{
+    [SerializeField]
+    private SwarmableArea _swarmArea;
+    [SerializeField]
+    private FishZone _fishZone;
+
+    private enum ActivationShapeRange
+    {
+        Sphere,
+        Box
+    };
+
+    [SerializeField]
+    private ActivationShapeRange activationShape = ActivationShapeRange.Box;
+
+    [SerializeField]
+    private float _fishDisperseRange;
+    [SerializeField]
+    private float _fishSwarmRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            _swarmArea.SwarmArea(_fishZone);
+        }
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            _swarmArea.StopSwarming();
+        }
+
+        float diff = (transform.position - SingleTons.GameController.Player.transform.position).magnitude;
+        if (diff * diff < _fishDisperseRange * _fishDisperseRange)
+        {
+            _swarmArea.StopSwarming();
+        }
+        else if (diff * diff < _fishSwarmRange * _fishSwarmRange)
+        {
+            _swarmArea.SwarmArea(_fishZone);
+        }
+        else
+        {
+            _swarmArea.StopSwarming();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        switch (activationShape)
+        {
+            case ActivationShapeRange.Sphere:
+                Gizmos.color = new Color(255, 165, 0);
+                Gizmos.DrawWireSphere(transform.position, _fishDisperseRange);
+                Gizmos.color = new Color(128, 0, 128);
+                Gizmos.DrawWireSphere(transform.position, _fishSwarmRange);
+                break;
+            case ActivationShapeRange.Box:
+                break;
+        }
+    }
+}
---
+++ Project Exposure/Assets/Scripts/Singletons/CollectionsManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionsManager : MonoBehaviour
+{
+    [SerializeField] [Range(0, 1)] private float _volume = 0.8f;
+    [SerializeField] [Range(0, 15)] private int _maxDistance = 8;
+
+    [HideInInspector] public List<GameObject> CollectableAudioSources = new List<GameObject>();
+    [HideInInspector] public Dictionary<string, AudioSource> CollectedAudioSources = new Dictionary<string, AudioSource>();
+
+    void Start()
+    {
+        SingleTons.CollectionsManager = this;
+
+        GameObject[] gos = GameObject.FindObjectsOfType<GameObject>();
+        for (int i = 0; i < gos.Length; i++)
+        {
+            if (gos[i].layer == 10)
+            {
+                AudioSource aSource = gos[i].GetComponent<AudioSource>();
+                aSource.spatialBlend = 1.0f;
+                aSource.volume = _volume;
+                aSource.maxDistance = _maxDistance;
+                aSource.rolloffMode = AudioRolloffMode.Custom;
+                aSource.loop = true;
+
+                if (gos[i].tag == "Collectable")
+                    CollectableAudioSources.Add(gos[i]);
+            }
+        }
+    }
+
+    public bool IsCollected(string pGameObjectName)
+    {
+        foreach (string key in CollectedAudioSources.Keys)
+        {
+            if (key == pGameObjectName)
+                return true;
+        }
+
+        return false;
+    }
+
+    public void AddToCollection(GameObject pGameObject)
+    {
+        bool isAlreadyCollected = true;
+        for (int i = 0; i < CollectableAudioSources.Count; i++)
+        {
+            if (CollectableAudioSources[i] == pGameObject)
+            {
+                CollectedAudioSources.Add(pGameObject.name, pGameObject.GetComponent<AudioSource>());
+                isAlreadyCollected = false;
+                Debug.Log(string.Format("New audio sample found: {0}", pGameObject.transform.name.ToUpper()));
+                break;
+            }
+        }
+
+        if (isAlreadyCollected)
+        {
+            print("Object already collected!");
+            return;
+        }
+
+        for (int i = CollectableAudioSources.Count - 1; i >= 0; i--)
+            if (CollectableAudioSources[i].name == pGameObject.name)
+                CollectableAudioSources.RemoveAt(i);
+    }
+
+    public int GetMaxDistance
+    {
+        get { return _maxDistance; }
+    }
+}
