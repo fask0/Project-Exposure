46674339c85698df04afd857d8e6ad4a1093c20b
rpijpker01 <rimme.pijpker@gmail.com>
2019-05-22 12:59:50 +0200

    thingy

--- Project Exposure/Assets/Scripts/Highscore/HighscoreManager.cs
+++ Project Exposure/Assets/Scripts/Highscore/HighscoreManager.cs
@@ -2,22 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
 
 public class HighscoreManager : MonoBehaviour
 {
 
-    private string _path = "Assets\\Highscores";
+    private string _path = "Assets/Highscores/";
 
     // Start is called before the first frame update
     void Start()
     {
-        File.WriteAllText(_path, "");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.F7))
+        {
+            CreateFile("" + DateTime.Today.Day + "-" + DateTime.Today.Month + "-" + DateTime.Today.Year);
+        }
+    }
 
+    public void CreateFile(string fileName)
+    {
+        File.Create(_path + fileName + ".txt");
     }
 
     public void SaveScore()
