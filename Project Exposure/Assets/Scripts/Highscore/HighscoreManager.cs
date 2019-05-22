5dc0083534c047a84a5c145536dee28f62f953c5
rpijpker01 <rimme.pijpker@gmail.com>
2019-05-22 12:36:42 +0200

    Overwrite maincanvas prefab

---
+++ Project Exposure/Assets/Scripts/Highscore/HighscoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class HighscoreManager : MonoBehaviour
+{
+
+    private string _path = "Assets\\Highscores";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        File.WriteAllText(_path, "");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SaveScore()
+    {
+
+    }
+}
