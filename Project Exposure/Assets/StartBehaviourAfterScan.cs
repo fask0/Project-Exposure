5ae61e7fd774a4f52c2034f468fe2b5a0b722cb3
rpijpker01 <rimme.pijpker@gmail.com>
2019-06-08 20:55:05 +0200

    Tutorial level done, dont @ me : - )

--- Project Exposure/Assets/StartBehaviourAfterScan.cs
+++ Project Exposure/Assets/StartBehaviourAfterScan.cs
@@ -1,21 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class StartBehaviourAfterScan : MonoBehaviour
 {
     private FishBehaviour fishBehaviour;
 
+    [SerializeField]
+    private bool _startBehaviour = true;
+    [SerializeField]
+    private UnityEvent afterScanEvent;
+
     // Start is called before the first frame update
     void Start()
     {
-        fishBehaviour = GetComponent<FishBehaviour>();
-        SingleTons.SoundWaveManager.onFishScanEvent += StartFishBehaviour;
+        if(_startBehaviour)
+            fishBehaviour = GetComponent<FishBehaviour>();
+        if (fishBehaviour != null)
+        {
+            SingleTons.SoundWaveManager.onFishScanEvent += StartFishBehaviour;
+        }
+
+        SingleTons.SoundWaveManager.scanEvents.Add(gameObject, afterScanEvent);
     }
 
     private void OnDisable()
     {
         SingleTons.SoundWaveManager.onFishScanEvent -= StartFishBehaviour;
+        SingleTons.SoundWaveManager.scanEvents.Remove(gameObject);
     }
 
     void StartFishBehaviour(GameObject obj)
